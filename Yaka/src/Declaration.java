ccfd3c12d16d55baad97dcdf61a7689b1ab2dba6
ithek <kev.thek@gmail.com>
2014-02-27 11:50:31 +0100

    Delete Enum.java, ajout enum Type, enum Nature, Declaration.java

--- Yaka/src/Declaration.java
+++ Yaka/src/Declaration.java
@@ -1,9 +1,29 @@
-
-/**
- * Gestion des declarations de variables et constantes (appels vers Ident, etc.)
- * @author paulriviere
- *
- */
 public class Declaration {
+	private String nom;
+	private Boolean isInteger;	
 
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	//Constante entier
+	public void declConst(Integer val) {
+		Yaka.tabIdent.creationIdent(Nature.CONSTANTE,this.nom,Type.ENTIER,val);
+	}
+
+	//Constante booleen
+	public void declConst(Boolean val) {
+		Yaka.tabIdent.creationIdent(Nature.CONSTANTE,this.nom,Type.BOOLEEN,val);
+	}
+
+	//Entier ou booleen
+	public void setIsInteger(Boolean b) {
+		isInteger = b;
+	}
+
+	//Variable
+	public void declVar(String nom) {
+		if(isInteger) Yaka.tabIdent.creationIdent(Nature.VARIABLE,nom,Type.ENTIER,null);
+		else Yaka.tabIdent.creationIdent(Nature.VARIABLE,nom,Type.BOOLEEN,null);
+	}
 }
---
+++ Yaka/src/Nature.java
@@ -0,0 +1,4 @@
+public enum Nature {
+	CONSTANTE,
+	VARIABLE;
+}
