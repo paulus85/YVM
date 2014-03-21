7bd55711299b664607da00c1a2bc70866523844b
ithek <kev.thek@gmail.com>
2014-03-21 11:10:35 +0100

    ExprNonBoolException

---
+++ Yaka/src/exceptions/ExprNonBoolException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class ExprNonBoolException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ExprNonBoolException(String msg) {
+		super(msg);
+	}
+}
